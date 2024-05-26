e8951a452292e93aad9985a1e3c600388ffb86be
Haris Sabanovic <hasa2303@student.miun.se>
2024-05-26 15:41:01 +0200

    hämtar jobb och visar dem på hemsidan

---
+++ public/js/main.js
@@ -0,0 +1,20 @@
+
+        // JavaScript för att hämta data från servern och visa den på sidan
+fetch('/jobs')
+    .then(response => response.json())
+    .then(jobs => {
+         const jobListDiv = document.getElementById('jobList');
+        jobs.forEach(job => {
+             const jobDiv = document.createElement('div');
+             jobDiv.classList.add('job');
+             jobDiv.innerHTML = `
+             <h3>${job.companyName}</h3>
+             <p><strong>Title:</strong> ${job.jobTitle}</p>
+             <p><strong>Start Date:</strong> ${job.startDate}</p>
+             <p><strong>End Date:</strong> ${job.endDate}</p>
+             <p><strong>Description:</strong> ${job.description}</p>
+             <button class="delete-btn" data-id="${job._id}">Delete</button>
+             `;
+            jobListDiv.appendChild(jobDiv);
+        });
+ });
